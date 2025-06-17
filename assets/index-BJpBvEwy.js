258eb26649a1b441637142552e57555bf03dc117
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-17 18:53:41 +0300

    Updates

--- assets/index-BJpBvEwy.js
+++ assets/index-BJpBvEwy.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-DFRZK_NH.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-CaUwMpo3.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

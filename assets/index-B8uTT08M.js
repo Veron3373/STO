d76490a218916acf645a1265f9b77eeea2cbdb27
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-18 10:27:07 +0300

    Updates

--- assets/index-B8uTT08M.js
+++ assets/index-B8uTT08M.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-lJHvdJBh.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BRXdW4bP.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

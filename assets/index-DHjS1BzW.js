b13895b896f6c1e0f910bdd49ea1d9a1a2e9baa3
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-18 10:29:44 +0300

    Updates

--- assets/index-DHjS1BzW.js
+++ assets/index-DHjS1BzW.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-BRXdW4bP.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-B4TTc4S3.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

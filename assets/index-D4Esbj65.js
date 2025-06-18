7d995f5f72c485f171912265b8f7d7164a457068
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-18 15:47:42 +0300

    Updates

--- assets/index-D4Esbj65.js
+++ assets/index-D4Esbj65.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-BboeMKY8.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BNJ1KSMm.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

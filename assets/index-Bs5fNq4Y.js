36fa4e4330bf4de63125c26da17b1aa98e68940a
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-18 10:11:25 +0300

    Updates

--- assets/index-Bs5fNq4Y.js
+++ assets/index-Bs5fNq4Y.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-20JANjPt.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-DoNgSlgI.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

26c297bed40d1461c6e34786d74e5d86553e008d
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-18 15:51:09 +0300

    Updates

--- assets/index-BjjDPZNn.js
+++ assets/index-BjjDPZNn.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-BNJ1KSMm.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-DyJGz6YO.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

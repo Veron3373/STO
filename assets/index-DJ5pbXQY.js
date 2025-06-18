9f7be5d7a799684ab4ca6a53c29cb65a22c055c0
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-18 09:10:56 +0300

    Updates

--- assets/index-DJ5pbXQY.js
+++ assets/index-DJ5pbXQY.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-DyLMuSyF.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BA9rP4Bg.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

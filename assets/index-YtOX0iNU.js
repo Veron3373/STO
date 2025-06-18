df48dfb0ced0fbde4f4e7047d5858ac7668ff6b5
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-18 15:44:24 +0300

    Updates

--- assets/index-YtOX0iNU.js
+++ assets/index-YtOX0iNU.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-CxT_W7if.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BboeMKY8.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

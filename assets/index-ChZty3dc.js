853952e17e380999cc7d43fd9ab9150a3837f7ac
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-18 10:33:31 +0300

    Updates

--- assets/index-ChZty3dc.js
+++ assets/index-ChZty3dc.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-B4TTc4S3.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BOaYX5un.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

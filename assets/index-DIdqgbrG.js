c2f43179b2356d5fc0352dd2dbc63efed674e504
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-20 18:32:28 +0300

    Updates

--- assets/index-DIdqgbrG.js
+++ assets/index-DIdqgbrG.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-BiOlJY09.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-C8-jIUVo.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

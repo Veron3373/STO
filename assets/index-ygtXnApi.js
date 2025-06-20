190129489e011c843eba4d7880dc03ae77fd4af7
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-20 18:24:34 +0300

    Updates

--- assets/index-ygtXnApi.js
+++ assets/index-ygtXnApi.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-B2o65fMg.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BiOlJY09.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

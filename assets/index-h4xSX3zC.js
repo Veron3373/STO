50dd200cd53ab5efebc4e4fe664ca2b722f0f0f2
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-20 18:36:23 +0300

    Updates

--- assets/index-h4xSX3zC.js
+++ assets/index-h4xSX3zC.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-C8-jIUVo.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-DHQNv1OW.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

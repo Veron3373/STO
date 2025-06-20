32331758b562f687a2ea6e78553ec2fa55107aa6
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-20 07:30:11 +0300

    Updates

--- assets/index-BnTPjugF.js
+++ assets/index-BnTPjugF.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-Dbl9BPz3.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-ZUugHIPO.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

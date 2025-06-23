fbfe1ae2fdfb3a6a0427e8b764bf7101e9a894b7
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-23 10:01:58 +0300

    Updates

--- assets/index-CHdte1Ay.js
+++ assets/index-CHdte1Ay.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-FaxsBFn_.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BtZS0Dwz.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

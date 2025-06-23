e7438e28b758f25d05b8bf3d9967274929c1013b
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-23 09:50:08 +0300

    Updates

--- assets/index-Cz0DMVTj.js
+++ assets/index-Cz0DMVTj.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-BJ2tctfN.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-CPfKlJHr.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

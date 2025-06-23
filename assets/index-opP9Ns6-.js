134dcbfb5cec2613ed9887ae5ddc4f8e60f48e85
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-23 13:54:56 +0300

    Updates

--- assets/index-opP9Ns6-.js
+++ assets/index-opP9Ns6-.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-Bd432f_5.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-C39crrhQ.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

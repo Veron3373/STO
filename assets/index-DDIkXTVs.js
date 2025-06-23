8ea26613d61532a6f1eefb2dbed3fd395a945be5
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-23 09:51:48 +0300

    Updates

--- assets/index-DDIkXTVs.js
+++ assets/index-DDIkXTVs.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-CPfKlJHr.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-DuwDZI7z.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

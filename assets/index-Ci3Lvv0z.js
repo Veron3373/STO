79e74bc8bbc0f5640462ff191d508f8303ea228c
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-23 13:49:17 +0300

    Updates

--- assets/index-Ci3Lvv0z.js
+++ assets/index-Ci3Lvv0z.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-XOcU_IRL.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-kKqfmi7X.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

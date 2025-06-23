627edd1e39539996e731d050f872cb3ae169cd03
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-23 11:31:54 +0300

    Updates

--- assets/index-BE3XWgyn.js
+++ assets/index-BE3XWgyn.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-B2BPixOd.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-C18IZK-_.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

f3f825994977169a1f6de5b6aa61c08b19f693b4
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-23 13:38:15 +0300

    Updates

--- assets/index-C0EBmDpV.js
+++ assets/index-C0EBmDpV.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-XOcU_IRL.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BeLmrX60.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

ebe709c5e05150a779175b30ae7f2c6290554891
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-23 11:36:52 +0300

    Updates

--- assets/index-NCFxQTcz.js
+++ assets/index-NCFxQTcz.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-BUoEJlEN.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BrFGJWtc.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

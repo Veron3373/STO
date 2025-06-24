a786f448ebf8d2ea21aaeb7670f02df563ec460c
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-24 15:23:01 +0300

    Updates

--- assets/index-DX9Df06N.js
+++ assets/index-DX9Df06N.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-Dcn36bs3.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-Dij7FAQa.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

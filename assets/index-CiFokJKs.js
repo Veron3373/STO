823d94d11ed91a675a3f6ccf1dacbece00c092ea
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-24 13:14:30 +0300

    Updates

--- assets/index-CiFokJKs.js
+++ assets/index-CiFokJKs.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-COh8w6i-.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-B97N5S1Y.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

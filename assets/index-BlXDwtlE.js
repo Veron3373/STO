bf5e10d3f400773668451a7e16511b316133d7c2
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-24 15:42:31 +0300

    Updates

--- assets/index-BlXDwtlE.js
+++ assets/index-BlXDwtlE.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-61xT_gZj.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-DHMyLy4F.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

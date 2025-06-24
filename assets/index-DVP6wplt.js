407cbcf8097c498baa683eeb46f92689fc635339
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-24 10:50:53 +0300

    Updates

--- assets/index-DVP6wplt.js
+++ assets/index-DVP6wplt.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-DQp2uofH.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-D1ZX_K06.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

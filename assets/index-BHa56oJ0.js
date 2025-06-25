7909f13559bb6216fb72cf08e36a85df32d8898e
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-25 16:28:33 +0300

    Updates

--- assets/index-BHa56oJ0.js
+++ assets/index-BHa56oJ0.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-b1VqPtTy.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-B_ND-TGl.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

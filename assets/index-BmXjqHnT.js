1c9ee5157de730bc92227019add2c3e3ed4cd147
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-25 16:40:13 +0300

    Updates

--- assets/index-BmXjqHnT.js
+++ assets/index-BmXjqHnT.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-B_ND-TGl.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-CWx74O7-.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

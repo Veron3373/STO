bbb5db9322bacfab2bdec65298815af2f3c47d4a
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-25 16:16:55 +0300

    Updates

--- assets/index-Di0nC2eO.js
+++ assets/index-Di0nC2eO.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-BRYH59Du.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-DP7-08K4.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

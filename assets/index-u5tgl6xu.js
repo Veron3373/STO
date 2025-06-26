59baf372332e9d0dd6860a81d7c9a19665059fd2
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-26 18:32:00 +0300

    Updates

--- assets/index-u5tgl6xu.js
+++ assets/index-u5tgl6xu.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-BuvG9Uv7.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-CiJhHpBt.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

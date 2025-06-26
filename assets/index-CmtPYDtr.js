424fe655498abd336dd8cfb3dcf091bc8dcfa279
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-26 18:41:51 +0300

    Updates

--- assets/index-CmtPYDtr.js
+++ assets/index-CmtPYDtr.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-BlUInzU-.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BPwiEkWP.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

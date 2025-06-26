d4864d620e362d1808c2aa80a6864d7db8066fca
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-26 17:11:14 +0300

    Updates

--- assets/index-DDfRneGR.js
+++ assets/index-DDfRneGR.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-C5wReD9t.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-B4EeRbvA.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

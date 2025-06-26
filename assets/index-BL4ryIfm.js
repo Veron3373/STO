135c4bb00ea521b70662e9da2f34d898442b6d27
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-26 18:49:34 +0300

    Updates

--- assets/index-BL4ryIfm.js
+++ assets/index-BL4ryIfm.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-DlES1YrX.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-D3TOSsU7.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

f6cba849e8b10d4d7a8a0d934a743ada6619982b
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-26 11:02:54 +0300

    Updates

--- assets/index-ElYXnVLh.js
+++ assets/index-ElYXnVLh.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-DqHtxUcP.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-D2CbWNMV.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

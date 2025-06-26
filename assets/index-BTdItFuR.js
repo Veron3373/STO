8538c67f5489f38da7194285adbdfc7c3a5b466c
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-26 14:29:15 +0300

    Updates

--- assets/index-BTdItFuR.js
+++ assets/index-BTdItFuR.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-BkY5zhxo.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BC6Ziw-X.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

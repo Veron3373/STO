99b07f6abe57d5c0b68b9d959d4fa90c20cc57d5
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-27 11:02:43 +0300

    Updates

--- assets/index-Ba1kGx2T.js
+++ assets/index-Ba1kGx2T.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-Dw8ICeY8.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-C8CJ_ML6.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

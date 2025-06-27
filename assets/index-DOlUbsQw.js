0d964c92d6b539e36877ce7014abe2806896cd6e
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-27 11:24:08 +0300

    Updates

--- assets/index-DOlUbsQw.js
+++ assets/index-DOlUbsQw.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-WQHR0Usc.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-CkUGXe63.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

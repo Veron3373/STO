e17476ea2d28dbb0b49ba33ff66ed958b3d3bc16
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-27 14:06:01 +0300

    Updates

--- assets/index-CqOX86iK.js
+++ assets/index-CqOX86iK.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-CJvudIc1.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BVPhtWqi.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

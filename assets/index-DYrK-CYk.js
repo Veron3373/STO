6dc3d34477831e8dd12945130062c77697a32362
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-27 07:53:08 +0300

    Updates

--- assets/index-DYrK-CYk.js
+++ assets/index-DYrK-CYk.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-D3TOSsU7.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-DIAj0Bjr.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

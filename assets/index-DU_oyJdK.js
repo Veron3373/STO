5b178f6573e543a70a959e76e178901a6efcbb19
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-27 13:52:58 +0300

    Updates

--- assets/index-DU_oyJdK.js
+++ assets/index-DU_oyJdK.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-g_ed-Xy_.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-dcc4bHe8.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

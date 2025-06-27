2c0e4ec28ef8d2775f3ccb8b8343b8b2da59ab0c
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-27 14:10:20 +0300

    Updates

--- assets/index-DgMYi4jC.js
+++ assets/index-DgMYi4jC.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-B9qqI_84.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-BUq38TOO.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

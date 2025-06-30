c5a510094e4142b4d9c8e6d9c7d2a29a29e19f47
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-06-30 10:12:53 +0300

    Updates

--- assets/index-p863368N.js
+++ assets/index-p863368N.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-I0bIPG78.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-DXYxqt28.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *

f819007acfc0dd6523e84c0b86e40e56b3e5b97f
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-07-01 11:19:28 +0300

    Updates

--- assets/pidtverdutu_sberihannya_zakaz_naryad-Cg-AAbzo.js
+++
@@ -1,7 +0,0 @@
-import{g as r,u as d}from"./main-BZMxu1zJ.js";import"./supabaseClient-B08L58Py.js";const l="save-prompt-modal-create";console.log(r,d);function v(){const e=document.createElement("div");e.id=l,e.className="modal-overlay-create-sakaz_narad",e.style.display="none";const n=document.createElement("div");return n.className="modal-content-create-sakaz_narad",n.innerHTML=`
-    <p>Зберегти зміни?</p>
-    <div class="save-buttons">
-      <button id="save-confirm" class="btn-save-confirm">Так</button>
-      <button id="save-cancel" class="btn-save-cancel">Ні</button>
-    </div>
-  `,e.appendChild(n),e}function u(){return new Promise(e=>{const n=document.getElementById(l);if(!n)return e(!1);n.style.display="flex";const t=document.getElementById("save-confirm"),a=document.getElementById("save-cancel"),o=()=>{n.style.display="none",t.removeEventListener("click",c),a.removeEventListener("click",s)},c=()=>{o(),e(!0)},s=()=>{o(),e(!1)};t.addEventListener("click",c),a.addEventListener("click",s)})}export{v as createSavePromptModal,l as savePromptModalId,u as showSavePromptModal};

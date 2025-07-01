3f52a0f991d4fb6e09196873560488629fb43fb3
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-07-01 12:31:23 +0300

    Updates

--- assets/pidtverdutu_sberihannya_zakaz_naryad-CxjxBjrf.js
+++
@@ -1,7 +0,0 @@
-import{g as r,u as d}from"./main-6oI7NRkV.js";import"./supabaseClient-B08L58Py.js";const t="save-prompt-modal";function u(){const e=document.createElement("div");e.id=t,e.className="",e.style.display="none";const n=document.createElement("div");return n.className="modal-content-save",n.innerHTML=`
-    <p>Зберегти зміни?</p>
-    <div class="save-buttons">
-      <button id="save-confirm" class="btn-save-confirm">Так</button>
-      <button id="save-cancel" class="btn-save-cancel">Ні</button>
-    </div>
-  `,e.appendChild(n),e}function p(){return new Promise(e=>{const n=document.getElementById(t);if(!n)return console.warn("Модальне вікно не знайдено:",t),e(!1);n.classList.add("active"),n.style.display="flex";const o=document.getElementById("save-confirm"),s=document.getElementById("save-cancel"),a=()=>{n.classList.remove("active"),n.style.display="none",o.removeEventListener("click",c),s.removeEventListener("click",l)},c=()=>{a();const i=r();console.log("✅ Значення форми:",i),console.log("✅ userConfirmation:",d),e(!0)},l=()=>{a(),e(!1)};o.addEventListener("click",c),s.addEventListener("click",l)})}export{u as createSavePromptModal,t as savePromptModalId,p as showSavePromptModal};

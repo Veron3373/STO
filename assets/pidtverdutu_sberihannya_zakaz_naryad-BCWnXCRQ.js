d26adfdf8b299157ed7b422af5d4b660c72d4f54
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-07-02 14:10:16 +0300

    Updates

--- assets/pidtverdutu_sberihannya_zakaz_naryad-BCWnXCRQ.js
+++
@@ -1,7 +0,0 @@
-import{g as i}from"./main-ryBglNE6.js";import"./supabaseClient-COcdlTl_.js";const a="save-prompt-modal-create";function v(){const t=document.createElement("div");t.id=a,t.className="modal-create-overlay",t.style.display="none";const e=document.createElement("div");return e.className="modal-content-save",e.innerHTML=`
-    <p>Створити заказ наряд?</p>
-    <div class="save-buttons">
-      <button id="save-confirm-create" class="btn-save-confirm">Так</button>
-      <button id="save-cancel-create" class="btn-save-cancel">Ні</button>
-    </div>
-  `,t.appendChild(e),t}function u(){return new Promise(t=>{const e=document.getElementById(a);if(!e)return console.warn("❌ Модальне вікно не знайдено:",a),t(!1);e.classList.add("active"),e.style.display="flex";const c=e.querySelector("#save-confirm-create"),o=e.querySelector("#save-cancel-create"),s=()=>{e.classList.remove("active"),e.style.display="none",c.removeEventListener("click",r),o.removeEventListener("click",l)},r=()=>{try{const n=i();console.log("🆕 Дані нового запису:",JSON.stringify(n,null,2)),s(),t(!0)}catch(n){console.error("🚨 Помилка у onConfirm:",n),t(!1)}},l=()=>{s(),t(!1)};c.addEventListener("click",r),o.addEventListener("click",l)})}export{v as createSaveModalCreate,a as saveModalIdCreate,u as showSaveModalCreate};

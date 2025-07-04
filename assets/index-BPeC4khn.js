631197a778d0699df29bf6ec9944de5af248f2f2
Veron3373 <102152606+Veron3373@users.noreply.github.com>
2025-07-04 16:31:36 +0300

    Updates

--- assets/index-BPeC4khn.js
+++ assets/index-BPeC4khn.js
@@ -1,4 +1,4 @@
-import{s as qn}from"./supabaseClient-CxVC3eQh.js";var $c={exports:{}},oe={};/**
+import{s as qn}from"./supabaseClient-COcdlTl_.js";var $c={exports:{}},oe={};/**
  * @license React
  * react-jsx-runtime.production.js
  *
